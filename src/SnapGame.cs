854e3925d826ad2f9eb37b9424d1bc3d424a8478
al-aminju <mdalamin.or136@gmail.com>
2025-08-15 15:20:02 +0600

    fix position of the cards to be over background

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -52,6 +52,8 @@ namespace CardGames
     			myGame.Score(1), Color.RoyalBlue, "GameFont", 0, 40);
 
 				SwinGame.DrawCell(SwinGame.BitmapNamed("Cards"), top.CardIndex, 350, 50);
+				SwinGame.DrawCell (SwinGame.BitmapNamed 
+				("Cards"), top.CardIndex, 521, 153);
 
 			}
 			else
